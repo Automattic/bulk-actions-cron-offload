e9586c650e368bf92508ab9c6f0aee82fbc4c90c
Erick Hitter <services@ethitter.com>
2017-02-24 19:42:10 -0800

    Start with a class for the various intercepts
    Individual classes will process the various bulk requests

---
+++ bulk-edit-cron-offload.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ Plugin Name: Offload Bulk Edit to Cron
+ Plugin URI: https://vip.wordpress.com/
+ Description: Process Bulk Edit requests using Cron
+ Author: Erick Hitter, Automattic
+ Version: 1.0
+ Text Domain: automattic-bulk-edit-cron-offload
+ */
+
+namespace Automattic\WP\Bulk_Edit_Cron_Offload;
+
+require __DIR__ . '/includes/class-main.php';
---
+++ includes/class-main.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Automattic\WP\Bulk_Edit_Cron_Offload;
+
+class Main {
+	/**
+	 *
+	 */
+	static function load() {
+		// filters!
+	}
+}
+
+Main::load();
