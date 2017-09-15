9d51824ac2d6f1223c01ac7583b0ea2997880132
Erick Hitter <git-contrib@ethitter.com>
2017-09-14 19:00:35 -0700

    Keep the notice keys out of pagination and such

--- includes/trait-bulk-actions.php
+++ includes/trait-bulk-actions.php
@@ -18,11 +18,25 @@ trait Bulk_Actions {
 		add_action( 'admin_notices', array( __CLASS__, 'admin_notices' ) );
 		add_filter( 'posts_where', array( __CLASS__, 'hide_posts' ), 999, 2 );
 
+		add_filter( 'removable_query_args', array( __CLASS__, 'remove_notice_arg' ) );
+
 		self::register_extra_hooks();
 	}
 
 	/**
-	 *
+	 * Some methods may need extra hooks
 	 */
 	public static function register_extra_hooks() {}
+
+	/**
+	 * Strip the custom notice key, otherwise it turns up in pagination and other unwanted places.
+	 *
+	 * @param array $args Array of one-time query args
+	 * @return array
+	 */
+	public static function remove_notice_arg( $args ) {
+		$args[] = self::ADMIN_NOTICE_KEY;
+
+		return $args;
+	}
 }
